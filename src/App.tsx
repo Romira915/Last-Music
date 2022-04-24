8ad1f21b7bbd409922543a6e6b9bfbdaa2b00159
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-04-24 14:32:28 +0900

    remove unused deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,50 @@
-import { useState } from "react";
-import "./App.css";
-import logo from "./logo.svg";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider as MaterialThemeProvider } from "@material-ui/core/styles";
+import { useCallback, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import AudioPlayerPanel from "./components/AudioPlayer/AudioPlayer";
+import LibraryPanel from "./components/LibraryPanel";
+import {
+  changeTheme,
+  GeneralSettings,
+} from "./slice/settings/generalSettingsSlice";
+import { RootState, useAppDispatch } from "./store";
+import { StyleApp } from "./styles/style";
+import { ThemeEnum, themeFromEnum } from "./theme/Theme";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = (props) => {
+  const dispatch = useAppDispatch();
+  const themeEnum = useSelector<RootState, GeneralSettings>(
+    (state) => state.settings.generalSettings
+  ).theme;
+  const theme = useMemo(() => themeFromEnum(themeEnum), [themeEnum]);
+
+  const onThemeChange = useCallback(() => {
+    dispatch(
+      changeTheme(
+        themeEnum === ThemeEnum.Light ? ThemeEnum.Dark : ThemeEnum.Light
+      )
+    );
+  }, [themeEnum]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <MaterialThemeProvider theme={theme}>
+      <StyledThemeProvider theme={theme}>
+        <CssBaseline>
+          <StyleApp>
+            {/* <Button onClick={onThemeChange}>
+                            ダークモード (撤去予定)
+                        </Button> */}
+            {/* {props.children} */}
+            <AudioPlayerPanel />
+
+            <LibraryPanel />
+          </StyleApp>
+        </CssBaseline>
+      </StyledThemeProvider>
+    </MaterialThemeProvider>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,56 +1,12 @@
-import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider as MaterialThemeProvider } from "@material-ui/core/styles";
-import React, { useCallback, useMemo } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import AudioPlayerPanel from "./components/AudioPlayer/AudioPlayer";
-import LibraryPanel from "./components/LibraryPanel";
-import {
-  changeTheme,
-  GeneralSettings
-} from "./slice/settings/generalSettingsSlice";
-import store, { persistor, RootState, useAppDispatch } from "./store";
-import { StyleApp } from "./styles/style";
-import { ThemeEnum, themeFromEnum } from "./theme/Theme";
+import App from "./App";
+import store, { persistor } from "./store";
 
 const show = () => {
   console.log("Button Click");
-};
-
-const App: React.FC = (props) => {
-  const dispatch = useAppDispatch();
-  const themeEnum = useSelector<RootState, GeneralSettings>(
-    (state) => state.settings.generalSettings
-  ).theme;
-  const theme = useMemo(() => themeFromEnum(themeEnum), [themeEnum]);
-
-  const onThemeChange = useCallback(() => {
-    dispatch(
-      changeTheme(
-        themeEnum === ThemeEnum.Light ? ThemeEnum.Dark : ThemeEnum.Light
-      )
-    );
-  }, [themeEnum]);
-
-  return (
-    <MaterialThemeProvider theme={theme}>
-      <StyledThemeProvider theme={theme}>
-        <CssBaseline>
-          <StyleApp>
-            {/* <Button onClick={onThemeChange}>
-                            ダークモード (撤去予定)
-                        </Button> */}
-            {/* {props.children} */}
-            <AudioPlayerPanel />
-
-            <LibraryPanel />
-          </StyleApp>
-        </CssBaseline>
-      </StyledThemeProvider>
-    </MaterialThemeProvider>
-  );
 };
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
